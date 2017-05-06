246776205b48d52c7719734f0d95334c5ef8c779
Marc-Antoine <marc@Marc-Antoines-iMac.local>
2017-05-05 23:23:55 -0700

    Add dummy values to start/end of data so axes line up

--- plot.py
+++ plot.py
@@ -7,19 +7,64 @@ from bokeh.plotting import figure, show, output_file
 from bokeh.charts import Scatter
 from bokeh.models import HoverTool
 
-
 dateparse = lambda x: pandas.datetime.strptime(x, '%Y-%m-%d')
+strongdateparse = lambda x: pandas.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
 
-# Unroll Stronglifts data, merge with Strong data
+def whitelist_columns(df, columns):
+	for column in df.columns.values:
+		if column not in columns:
+			df.drop(column, axis=1, inplace=True)
 
-# Mark computed 1RM in all lift data
+# TODO: Unroll Stronglifts data, merge with Strong data
+# TODO: Mark computed 1RM in all lift data
 
-# Add dummy min begin and max end date to body composition and lift data
-
-# Read Fitbit data
 fitbit = pandas.read_csv('data/fitbit.csv', parse_dates=['dateTime'], date_parser=dateparse)
+caliper = pandas.read_csv('data/caliper.csv', parse_dates=['date'], date_parser=dateparse)
+strong = pandas.read_csv('data/strong.csv', parse_dates=['Date'], date_parser=strongdateparse)
 
+whitelist_columns(fitbit, ['dateTime', 'body-weight', 'body-fat'])
+
+# Data before this time is bad
 fitbit = fitbit[fitbit['dateTime'] > datetime(2015, 12, 28)]
+
+min_timestamp = (pandas.Series([fitbit['dateTime'].min(), caliper['date'].min(), strong['Date'].min()]).min())
+max_timestamp = (pandas.Series([fitbit['dateTime'].max(), caliper['date'].max(), strong['Date'].max()]).max())
+
+fitbit = fitbit.append(
+	pandas.DataFrame({
+		'dateTime': max_timestamp, 
+		'body-weight': fitbit['body-weight'].iloc[-1], 
+		'body-fat': fitbit['body-fat'].iloc[-1]
+	}, index=[0]))
+
+fitbit = fitbit.append(
+	pandas.DataFrame({
+		'dateTime': min_timestamp, 
+		'body-weight': fitbit['body-weight'].iloc[0], 
+		'body-fat': fitbit['body-fat'].iloc[0]
+	}, index=[0]))
+
+lifts = ['Bench Press', 'Military Press', 'Deadlift', 'Front Squat', 'Squat']
+
+for lift in lifts:
+	strong = strong.append(
+		pandas.DataFrame({
+			'Date': max_timestamp,
+			'Exercise Name': lift,
+			'lb': strong[strong['Exercise Name'] == lift]['lb'].iloc[-1],
+			'Reps': 1
+		}, index=[0])
+	)
+	strong = strong.append(
+		pandas.DataFrame({
+			'Date': min_timestamp,
+			'Exercise Name': lift,
+			'lb': strong[strong['Exercise Name'] == lift]['lb'].iloc[0],
+			'Reps': 1
+		}, index=[0])
+	)
+
+
 
 fitbit['body-weight-lbs'] = fitbit['body-weight'] * 2.20462
 fitbit['lean-mass'] = fitbit['body-weight-lbs'] - fitbit['body-weight-lbs'] * (fitbit['body-fat'] / 100.0)
@@ -27,13 +72,8 @@ fitbit['label'] = fitbit['dateTime'].apply(lambda x: x.strftime('%Y-%m-%d'))
 
 fitbit['lean-mass'] += 10
 
-# Read Caliper data
-caliper = pandas.read_csv('data/caliper.csv', parse_dates=['date'], date_parser=dateparse)
 caliper['lean-mass'] = caliper['weight'] - caliper['weight'] * (caliper['caliper-body-fat'] / 100.0)
 
-# Read Lift data
-strongdateparse = lambda x: pandas.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-strong = pandas.read_csv('data/strong.csv', parse_dates=['Date'], date_parser=strongdateparse)
 strong['1RM'] = strong['lb'] / (1.0278 - (0.0278 * strong['Reps']))
 
 p = figure(title="Body Composition", x_axis_type='datetime')
