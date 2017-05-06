d15a6521f4502993856a4424ca0ea4d6414f2024
Marc-Antoine <marc@Marc-Antoines-iMac.local>
2017-05-05 22:56:56 -0700

    Add fitbit data cleaning to script

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
+from datetime import datetime
+import csv
 import numpy as np
 import pandas
-
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, show, output_file
 from bokeh.charts import Scatter
@@ -9,8 +10,17 @@ from bokeh.models import HoverTool
 
 dateparse = lambda x: pandas.datetime.strptime(x, '%Y-%m-%d')
 
+# Unroll Stronglifts data, merge with Strong data
+
+# Mark computed 1RM in all lift data
+
+# Add dummy min begin and max end date to body composition and lift data
+
 # Read Fitbit data
 fitbit = pandas.read_csv('data/fitbit.csv', parse_dates=['dateTime'], date_parser=dateparse)
+
+fitbit = fitbit[fitbit['dateTime'] > datetime(2015, 12, 28)]
+
 fitbit['body-weight-lbs'] = fitbit['body-weight'] * 2.20462
 fitbit['lean-mass'] = fitbit['body-weight-lbs'] - fitbit['body-weight-lbs'] * (fitbit['body-fat'] / 100.0)
 fitbit['label'] = fitbit['dateTime'].apply(lambda x: x.strftime('%Y-%m-%d'))
@@ -26,7 +36,6 @@ strongdateparse = lambda x: pandas.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
 strong = pandas.read_csv('data/strong.csv', parse_dates=['Date'], date_parser=strongdateparse)
 strong['1RM'] = strong['lb'] / (1.0278 - (0.0278 * strong['Reps']))
 
-
 p = figure(title="Body Composition", x_axis_type='datetime')
 p.circle(fitbit['dateTime'], fitbit['body-weight-lbs'], color='#9999ff', legend="weight (lbs)")
 p.circle(fitbit['dateTime'], fitbit['lean-mass'], color='pink', legend="lean mass (lbs)")
@@ -37,7 +46,6 @@ p.grid[0].ticker.desired_num_ticks = 15
 
 p.circle(caliper['date'], caliper['weight'], color='blue')
 p.circle(caliper['date'], caliper['lean-mass'], color='red')
-
 
 lifts_to_plot = ['Bench Press', 'Military Press', 'Deadlift', 'Front Squat', 'Squat']
 lift_colors = ['black', 'orange', 'blue', 'red', 'green']
