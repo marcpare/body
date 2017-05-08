84c1050216ac5858d87e9cc8dcdfb26eabb9fe05
Marc-Antoine <marc@Marc-Antoines-iMac.local>
2017-05-07 19:11:56 -0700

    Set the axis range properly

--- plot.py
+++ plot.py
@@ -11,6 +11,7 @@ from bokeh.layouts import gridplot
 from bokeh.plotting import figure, show, output_file
 from bokeh.charts import Scatter
 from bokeh.models import HoverTool
+from bokeh.models.ranges import Range1d
 
 dateparse = lambda x: pandas.datetime.strptime(x, '%Y-%m-%d')
 strongdateparse = lambda x: pandas.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
@@ -33,53 +34,20 @@ fitbit = fitbit[fitbit['dateTime'] > datetime(2015, 12, 28)]
 min_timestamp = (pandas.Series([fitbit['dateTime'].min(), caliper['date'].min(), strong['Date'].min()]).min())
 max_timestamp = (pandas.Series([fitbit['dateTime'].max(), caliper['date'].max(), strong['Date'].max()]).max())
 
-fitbit = fitbit.append(
-	pandas.DataFrame({
-		'dateTime': max_timestamp, 
-		'body-weight': fitbit['body-weight'].iloc[-1], 
-		'body-fat': fitbit['body-fat'].iloc[-1]
-	}, index=[0]))
-
-fitbit = fitbit.append(
-	pandas.DataFrame({
-		'dateTime': min_timestamp, 
-		'body-weight': fitbit['body-weight'].iloc[0], 
-		'body-fat': fitbit['body-fat'].iloc[0]
-	}, index=[0]))
-
-lifts = ['Bench Press', 'Military Press', 'Deadlift', 'Front Squat', 'Squat']
-
-for lift in lifts:
-	strong = strong.append(
-		pandas.DataFrame({
-			'Date': max_timestamp,
-			'Exercise Name': lift,
-			'lb': strong[strong['Exercise Name'] == lift]['lb'].iloc[-1],
-			'Reps': 1
-		}, index=[0])
-	)
-	strong = strong.append(
-		pandas.DataFrame({
-			'Date': min_timestamp,
-			'Exercise Name': lift,
-			'lb': strong[strong['Exercise Name'] == lift]['lb'].iloc[0],
-			'Reps': 1
-		}, index=[0])
-	)
-
+fitbit['body-fat'] -= 7
 fitbit['body-weight-lbs'] = fitbit['body-weight'] * 2.20462
 fitbit['lean-mass'] = fitbit['body-weight-lbs'] - fitbit['body-weight-lbs'] * (fitbit['body-fat'] / 100.0)
 fitbit['fat-mass'] = fitbit['body-weight-lbs'] - fitbit['lean-mass']
 
 fitbit['label'] = fitbit['dateTime'].apply(lambda x: x.strftime('%Y-%m-%d'))
 
-fitbit['lean-mass'] += 10
-
 caliper['lean-mass'] = caliper['weight'] - caliper['weight'] * (caliper['caliper-body-fat'] / 100.0)
 
 strong['1RM'] = strong['lb'] / (1.0278 - (0.0278 * strong['Reps']))
 
-p = figure(title="Body Composition", x_axis_type='datetime')
+x_range = Range1d(min_timestamp, max_timestamp)
+
+p = figure(title="Body Composition", x_axis_type='datetime', x_range=x_range)
 
 body_weight_rolling_avg = pandas.rolling_mean(fitbit['body-weight-lbs'], window=7)
 lean_mass_rolling_avg = pandas.rolling_mean(fitbit['lean-mass'], window=7)
@@ -93,17 +61,14 @@ body_fat_delta_smoothed = pandas.rolling_mean(body_fat_delta, window=7)
 body_fat_lost = -1 * fat_mass_rolling_avg.diff(-1)
 body_fat_lost_weekly_sum = pandas.rolling_sum(body_fat_lost, window=14)
 
-p.circle(fitbit['dateTime'], body_weight_rolling_avg, color='#9999ff', legend="weight (lbs)")
-p.circle(fitbit['dateTime'], lean_mass_rolling_avg, color='pink', legend="lean mass (lbs)")
+p.circle(fitbit['dateTime'], body_weight_rolling_avg, color='blue', legend="weight (lbs)")
+p.circle(fitbit['dateTime'], lean_mass_rolling_avg, color='red', legend="lean mass (lbs)")
 
 p.legend.location = 'top_right'
 p.legend.background_fill_alpha = 0.25
 p.grid[0].ticker.desired_num_ticks = 15
 
-p.circle(caliper['date'], caliper['weight'], color='blue')
-p.circle(caliper['date'], caliper['lean-mass'], color='red')
-
-p4 = figure(title="Fat mass", x_axis_type='datetime')
+p4 = figure(title="Fat mass", x_axis_type='datetime', x_range=x_range)
 p4.circle(fitbit['dateTime'], fat_mass_rolling_avg, color='green', legend="fat mass (lbs)")
 p4.grid[0].ticker.desired_num_ticks = 15
 
@@ -123,7 +88,7 @@ lift_colors = ['black', 'orange', 'blue', 'red', 'green']
 lifts_to_plot = ['Front Squat', 'Deadlift']
 lift_colors = ['orange', 'black']
 
-p2 = figure(title="Lifts", x_axis_type='datetime')
+p2 = figure(title="Lifts", x_axis_type='datetime', x_range=x_range)
 
 for lift, color in zip(lifts_to_plot, lift_colors):
 	lift_data = strong[strong['Exercise Name'] == lift]
@@ -136,7 +101,7 @@ p2.legend.background_fill_alpha = 0.25
 lifts_to_plot = ['Bench Press', 'Military Press']
 lift_colors = ['black', 'orange']
 
-p3 = figure(title="Lifts", x_axis_type='datetime')
+p3 = figure(title="Lifts", x_axis_type='datetime', x_range=x_range)
 
 for lift, color in zip(lifts_to_plot, lift_colors):
 	lift_data = strong[strong['Exercise Name'] == lift]
@@ -145,4 +110,4 @@ for lift, color in zip(lifts_to_plot, lift_colors):
 p3.legend.location = 'bottom_left'
 p3.legend.background_fill_alpha = 0.25
 
-show(gridplot(p, p7, p6, p4, p2, p3, ncols=1, nrows=5, plot_width=800, plot_height=400))
+show(gridplot(p, p4, p2, p3, ncols=1, nrows=5, plot_width=800, plot_height=400))
